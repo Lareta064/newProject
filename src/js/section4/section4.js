33017e64b72a1c606ea95851a52bb8c99bf6ce7f
Lareta <lareta064@gmail.com>
2020-02-29 08:43:01 +0300

    анимация-лого-партнеров

--- src/js/section4/section4.js
+++ src/js/section4/section4.js
@@ -1,4 +1,15 @@
 $(document).ready(function () {
+	// animate
+	wow = new WOW({
+		boxClass: 'wow', // default
+		animateClass: 'animated', // default
+		mobile: true, // default
+		offset: 110, // через сколько пикселей сработает
+		live: true // для мобильных оставить или убрать анимацию
+	});
+	wow.init();
+	new WOW().init();
+	// end animated
 	$(function () {
 
 		var target_block = $(".partners"); // Ищем блок 
